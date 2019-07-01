2b88a845600f8900fea4541f6be99ef779691cb0
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2019-07-01 16:08:56 +0530

    process raw data from mock files; use user auth for twitter api; setup getUser function

--- lambdas/getTweetEngagement.js
+++ lambdas/getTweetEngagement.js
@@ -1,13 +1,5 @@
-require('dotenv').config();
-const Twitter = require('twitter');
-
+const client = require('./utils/twitter-client');
 const response = require('../mock-tweets.json');
-
-// const client = new Twitter({
-//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//     bearer_token: process.env.TWITTER_BEARER_TOKEN
-// });
 
 exports.handler = async function(event, context) {
     // const { screenName } = JSON.parse(event.body);
@@ -26,23 +18,22 @@ exports.handler = async function(event, context) {
         //     exclude_replies: false,
         //     include_rts: false,
         // };
-        // const res = await client.get('statuses/user_timeline', options);
-        // res.sort((t1, t2) => {
-        //     const d1 = new Date(t1.created_at);
-        //     const d2 = new Date(t2.created_at);
-        //     if (d1 < d2) return -1;
-        //     if (d1 > d2) return 1;
-        //     return 0;
-        // });
-        // const { trimmedTweets, favs, rets } = splitFavouritesRetweets(res);
+        // const response = await client.get('statuses/user_timeline', options);
+        response.sort((t1, t2) => {
+            const d1 = new Date(t1.created_at);
+            const d2 = new Date(t2.created_at);
+            if (d1 < d2) return -1;
+            if (d1 > d2) return 1;
+            return 0;
+        });
+        const { trimmedTweets, favs, rets } = splitFavouritesRetweets(response);
         return {
             statusCode: 200,
-            // body: JSON.stringify({
-            //     tweets: trimmedTweets,
-            //     favourites: favs,
-            //     retweets: rets
-            // }),
-            body: JSON.stringify(response),
+            body: JSON.stringify({
+                tweets: trimmedTweets,
+                favourites: favs,
+                retweets: rets
+            }),
             headers
         };
     } catch (err) {
---
+++ lambdas/getUserSearch.js
@@ -0,0 +1,36 @@
+const client = require('./utils/twitter-client');
+const response = require('../mock-users.json');
+
+exports.handler = async function (event, context) {
+    // const { screenName } = JSON.parse(event.body);
+    // console.log(screenName);
+    const headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    };
+    try {
+        // const options = {
+        //     q: 'trump',
+        //     count: 5
+        // };
+        // const response = await client.get('users/search', options);
+        const users = response.map(({ name, screen_name, profile_image_url_https }) => ({
+            name,
+            screen_name,
+            profile_url: profile_image_url_https
+        }));
+        return {
+            statusCode: 200,
+            body: JSON.stringify(users),
+            headers
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(err),
+            headers
+        };
+    }
+};
