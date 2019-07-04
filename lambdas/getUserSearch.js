f5220ed29df96cf42db6754ca8799b5147d21d53
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2019-07-04 14:06:27 +0530

    search user from querystring

--- lambdas/getUserSearch.js
+++ lambdas/getUserSearch.js
@@ -1,19 +1,18 @@
 const client = require('./utils/twitter-client');
-const response = require('../mock-users.json');
+// const response = require('../mock-users.json');
 
 exports.handler = async function(event, context) {
-    // const { query } = JSON.parse(event.body);
-    // console.log(query);
+    const { q: query } = event.queryStringParameters;
     const headers = {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     };
     try {
-        // const response = await client.get('users/search', {
-        //     q: query,
-        //     count: 5
-        // });
+        const response = await client.get('users/search', {
+            q: query,
+            count: 5
+        });
         const users = response.map(({ name, screen_name, profile_image_url_https }) => ({
             name,
             screen_name,
